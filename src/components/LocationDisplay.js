b5e1dc38c05720980f47260c51f4606f5c60ba3d
Ryan Papsin <ryanpapsin175@gmail.com>
2022-12-08 21:10:54 -0700

    still working

--- src/components/LocationDisplay.js
+++ src/components/LocationDisplay.js
@@ -5,12 +5,11 @@ class LocationDisplay extends React.Component {
     return (
       <>
         <p>
-          name:
           {this.props.name}
           {this.props.lat}
           {this.props.lon}
         </p>
-        <img alt="location" src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.props.lat},${this.props.lon}&zoom=<zoom>&size=<width>x<height>&format=<format>&maptype=<MapType>&markers=icon:<icon>|<latitude>,<longitude>&markers=icon:<icon>|<latitude>,<longitude>`}
+        <img alt="location" src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.props.lat},${this.props.lon}&zoom=14`}
         />
       </>
     )
